97b13521935f89d0a3ff5de6862c1f2cea522bea
Hira2003 <hirafuyu2003@gmail.com>
2026-06-03 23:36:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,17 @@ function showSemester(id, button){
 
     button.classList.add("active");
 }
+const themeBtn = document.getElementById("themeBtn");
+
+themeBtn.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark-mode");
+
+    if(document.body.classList.contains("dark-mode")){
+        themeBtn.textContent = "☀️ Light Mode";
+    }
+    else{
+        themeBtn.textContent = "🌙 Dark Mode";
+    }
+
+});
